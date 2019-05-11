0e746e707967ab800fe6862d519de09c184a6153
EC2 Default User <ec2-user@ip-172-31-28-245.ap-southeast-1.compute.internal>
2019-05-11 03:14:30 +0000

    pry導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,15 +4,17 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
   end
   
   def create
     Article.create(create_params)
+    binding.pry
   end
   
   
   private
   def create_params
-    params.require(:article).permit(:text,:image)
+    params.require(:article).permit(:title,:text,:image)
   end
 end
