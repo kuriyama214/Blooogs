4a9439621617207ed7e103d781821a4fc2b5801e
EC2 Default User <ec2-user@ip-172-31-28-245.ap-southeast-1.compute.internal>
2019-06-07 07:29:45 +0000

    ログインページ背景

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+    #ログイン時に必要なkeyを追加する
     
 
   def configure_permitted_parameters
